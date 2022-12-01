b258465d854871eef028c54690e9bf1f6dcde4ad
MOHAMMED-BE <Bensghirsbih19@gmail.com>
2022-12-01 19:16:03 +0100

    Add Gaming Library Section

---
+++ src/Data/GamingLibraryData.js
@@ -0,0 +1,11 @@
+import GamingLibrary_01 from "../assets/images/game-01.jpg"
+import GamingLibrary_02 from "../assets/images/game-02.jpg"
+import GamingLibrary_03 from "../assets/images/game-03.jpg"
+
+const GamingLibraryData = [
+    { id: "library_item_1", image: GamingLibrary_01, title: "Data 2", category: "Sandbox", date_added: "09/07/2022", hours_played: "692 H 32 Mins", download:"downloaded" },
+    { id: "library_item_2", image: GamingLibrary_02, title: "Fortnite", category: "Sandbox", date_added: "24/02/2022", hours_played: "763 H 83 Mins", download:"download" },
+    { id: "library_item_3", image: GamingLibrary_03, title: "CS-GO", category: "Sandbox", date_added: "14/10/2022", hours_played: "860 H 91 Mins", download:"downloaded" },
+]
+
+export default GamingLibraryData
